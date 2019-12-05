73d99af55fd02d66476d5f449f35c1d34a79d79c
mobin-zaman <38745180+mobin-zaman@users.noreply.github.com>
2019-12-05 09:51:29 +0600

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace fractal_generator.Database
+{
+    class DB
+    {
+
+        public static  MySqlDataReader ExecuteSql(String sql)
+        {
+
+            var dbCon = DBConnection.Instance();
+            MySqlDataReader reader = null;
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = sql;
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                reader = cmd.ExecuteReader();
+                dbCon.Close();
+            }
+            return reader;
+        }
+    }
+}
