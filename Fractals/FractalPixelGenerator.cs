7c947fec43c7fedf8403a02236e0b09e024e7bab
terminal <terminal@TERMINAL-PC>
2019-12-07 04:07:13 +0600

    list is showed, fractal.cs refactored

---
+++ Fractals/FractalPixelGenerator.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fractal_generator.Fractals
+{
+    class FractalPixelGenerator
+    {
+        public static List<List<int>> CreateMandleBot() //methods starting with create means it give the pixelList
+        {
+            int Width = 640; //(int)canvas.Width; //FIXME: how to get the canvas size?
+            int Height = 480; //(int)canvas.Height;
+
+            var tupleList = new List<List<int>> { }; //list for saving the co-ordinates and color code 
+
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    double a = (double) (x - (Width / 2)) / (double) (Width / 4);
+                    double b = (double) (y - (Height / 2)) / (double) (Height / 4);
+
+                    Complex c = new Complex(a, b);
+                    Complex z = new Complex(0, 0);
+
+                    int it = 0;
+                    do
+                    {
+                        it++;
+                        z.Square();
+                        z.Add(c);
+                        if (z.Magnitude() > 4.0) break;
+
+                    } while (it < 900);
+
+                    // bm.SetPixel(x, y, it < 100 ? Color.Black : Color.White);
+                    int color_code = ((it < 100 ? 1 : 0));
+                    tupleList.Add(new List<int> {x, y, color_code});
+
+                }
+            }
+
+            return tupleList;
+        }
+
+        public static List<List<int>> CreateSierpinskiGasket()
+        {
+            int WIDTH = 768;
+            int HEIGHT = 768;
+
+            List<List<int>> tupleList = new List<List<int>> { };
+            for (int y = 0; y < WIDTH; y++)
+            {
+                for(int x=0; x< HEIGHT; x++)
+                {
+                    if((x & (y-x)) ==0)
+                    {
+                        //DrawPoint(x+158-y/2, y+30, 1);
+                        tupleList.Add(new List<int> { x + ((WIDTH/2)+30) - y / 2, y + 30, 1 });
+                    }
+                }
+            }
+            return tupleList;
+        }
+        /* the util functions which will be used on the main thread,
+         this are kept here for the time being, will be moved soon,
+                 void DrawListRandom(List<List<int>> pixelList)
+        {
+            DispatcherTimer t = new DispatcherTimer();
+            pixelList = ShuffleList(pixelList);
+
+            t.Tick += t_drawPoints;
+            t.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            t.Start();
+
+
+            void t_drawPoints(object sender, EventArgs e)
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    var randomElement = pixelList[0];
+                    DrawPoint(randomElement[0], randomElement[1], randomElement[2]);
+                    pixelList.RemoveAt(0);
+                }
+
+            }
+        }
+        private List<E> ShuffleList<E>(List<E> inputList)
+        {
+            List<E> randomList = new List<E>();
+
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+                randomList.Add(inputList[randomIndex]); //add it to the new, random list
+                inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+            }
+
+            return randomList;
+          }
+         
+        void DrawListIteration(List<List<int>> pixelList)
+        {
+            DispatcherTimer t = new DispatcherTimer();
+            t.Tick += t_drawPoints;
+            t.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            t.Start();
+            
+               void t_drawPoints(object sender, EventArgs e)
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    var firstElement = pixelList[0];
+                    DrawPoint(firstElement[0], firstElement[1], firstElement[2]);
+                    pixelList.RemoveAt(0);
+                }
+
+            }
+        }
+
+        void DrawPoint(int x, int y, int choice) //blue background, yellow middle purple dots
+        {
+                     Task.Run(() =>
+                       { 
+                     this.Dispatcher.Invoke(() =>
+
+             {
+            int dotSize = 2;
+               Ellipse currentDot = new Ellipse();
+               if (choice == 0)
+               {
+                int random = new Random().Next(1);
+                if (random == 0)
+                {
+                    currentDot.Stroke = new SolidColorBrush(Colors.DarkOrange);
+                    currentDot.Fill = new SolidColorBrush(Colors.DarkOrange);
+                }
+                else
+                {
+                    currentDot.Stroke = new SolidColorBrush(Colors.OrangeRed);
+                    currentDot.Fill = new SolidColorBrush(Colors.OrangeRed);
+                }
+               }
+               else
+               {
+                int random = new Random().Next(1);
+                if (random == 0)
+                {
+                    currentDot.Stroke = new SolidColorBrush(Colors.DarkBlue);
+                    currentDot.Fill = new SolidColorBrush(Colors.DarkBlue);
+                }
+                else
+                {
+                    currentDot.Stroke = new SolidColorBrush(Colors.Blue);
+                    currentDot.Fill = new SolidColorBrush(Colors.Blue);
+                } 
+               }
+               currentDot.StrokeThickness = 1;
+                //   Canvas.SetZIndex(currentDot, 3);
+                currentDot.Height = dotSize;
+               currentDot.Width = dotSize;
+               currentDot.Margin = new Thickness(x, y, 0, 0); // Sets the position.
+                canvas.Children.Add(currentDot);
+           });
+       });
+         }
+
+        
+    
+
+        void DrawSierpinskiGasket(List<List<int>> pixelList)
+        {
+            DispatcherTimer t = new DispatcherTimer();
+            t.Tick += t_drawPoints;
+            t.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            t.Start();
+
+            void t_drawPoints(object sender, EventArgs e)
+            {
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var firstElement = pixelList[0];
+                        DrawPoint(firstElement[0], firstElement[1], firstElement[2]);
+                        pixelList.RemoveAt(0);
+                    }
+                }
+                catch (Exception)
+                {
+                    t.Stop();
+                }
+
+
+            }
+        }
+
+       
+*/
+    }
+}
--- Model/fractal.cs
+++ Model/fractal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using fractal_generator.Database;
@@ -15,6 +16,7 @@ namespace fractal_generator.Model
         private String name;
         private String description;
         private String thumbUrl;
+        private BitmapImage imageData;
 
         public Fractal(MySqlDataReader reader)
         {
@@ -24,31 +26,42 @@ namespace fractal_generator.Model
             thumbUrl = reader.GetString(3);
         }
 
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public String Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public String Description
+        {
+            get { return this.Description; }
+            set { this.description = value; }
+        }
+
+        public BitmapImage ImageData
+        {
+            get { return this.LoadImage(); }
+        }
+
+
+
         public Fractal()
         {
 
         }
 
 
-        public int getId()
-        {
-            return id;
-        }
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public String getDescription()
-        {
-            return description;
-        }
 
         public String getThumbUrl()
         {
             String parentPath = GetParent(Directory.GetCurrentDirectory(), "fractal_generator");
-            string[] paths = {parentPath, "Model", "Image", thumbUrl};
+            string[] paths = {parentPath, "Model", "Images", thumbUrl};
             String imagePath = Path.Combine(paths);
             return imagePath;
 
@@ -71,6 +84,11 @@ namespace fractal_generator.Model
             return this.GetParent(dir.Parent.FullName, parentName);
         }
 
+        private BitmapImage LoadImage()
+        {
+            return new BitmapImage(new Uri(getThumbUrl()));
+        }
+
         public  static List<Fractal> GetFractalList()
         {
 
@@ -86,5 +104,7 @@ namespace fractal_generator.Model
             return fractalList;
             
         }
+
+        //public static Uri 
     }
 }
